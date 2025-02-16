b11c92c74732b78812db437909c5100317e13a22
Florian Meißner <developer@mystler.eu>
2025-02-16 16:32:39 +0100

    Store and reload custom map data

--- src/lib/types/MapData.svelte.ts
+++ src/lib/types/MapData.svelte.ts
@@ -31,21 +31,67 @@ export class MapData {
   Camera: CameraData = $state({ lookAtSystem: "", lookAt: [0, 0, 0], distance: 100 });
 
   // Setup Helpers
-  addFaction({ name = "", displayName = "", color = randomColor() } = {}) {
+  addFaction({ name = "", displayName = "", color = randomColor(), visible = true } = {}) {
     if (!displayName) displayName = name;
-    this.Factions.push({ name, displayName, color, visible: true });
+    this.Factions.push({ name, displayName, color, visible });
   }
-  addSystem({ name = "", displayName = "", color = randomColor() } = {}) {
+  addSystem({ name = "", displayName = "", color = randomColor(), visible = true } = {}) {
     if (!displayName) displayName = name;
-    this.Systems.push({ name, displayName, color, visible: true });
+    this.Systems.push({ name, displayName, color, visible });
   }
-  addSphere({ name = "", type = "Fortified" as SphereType, color = randomColor() } = {}) {
-    this.Spheres.push({ name, type, color, visible: true });
+  addSphere({
+    name = "",
+    type = "Fortified" as SphereType,
+    color = randomColor(),
+    visible = true,
+  } = {}) {
+    this.Spheres.push({ name, type, color, visible });
   }
 
   sortAll() {
     this.Factions.sort((a, b) => a.displayName.localeCompare(b.displayName));
     this.Systems.sort((a, b) => a.displayName.localeCompare(b.displayName));
     this.Spheres.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  /**
+   * Select what to store when serializing map data.
+   */
+  toJSON(): Partial<MapData> {
+    return {
+      Factions: this.Factions,
+      Systems: this.Systems,
+      Spheres: this.Spheres,
+      Camera: this.Camera,
+    };
+  }
+
+  /**
+   * Safely reconstruct map data from JSON storage by respecting defaults if data is missing.
+   */
+  static fromJSON(json: string): MapData {
+    const obj = JSON.parse(json);
+    const data = new MapData();
+
+    if ("Factions" in obj && obj.Factions instanceof Array) {
+      for (const faction of obj.Factions) {
+        data.addFaction(faction);
+      }
+    }
+    if ("Systems" in obj && obj.Systems instanceof Array) {
+      for (const system of obj.Systems) {
+        data.addSystem(system);
+      }
+    }
+    if ("Spheres" in obj && obj.Spheres instanceof Array) {
+      for (const sphere of obj.Spheres) {
+        data.addSphere(sphere);
+      }
+    }
+    if ("Camera" in obj) {
+      Object.assign(data.Camera, obj.Camera);
+    }
+
+    return data;
   }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import Map from "$lib/components/Map.svelte";
   import { MapData } from "$lib/types/MapData.svelte";
   import MapSetup from "$lib/components/MapSetup.svelte";
-  //import { browser } from "$app/environment";
+  import { browser } from "$app/environment";
 
   let mapData = $state(new MapData());
   let setupComplete = $state(false);
@@ -11,12 +11,19 @@
     setupComplete = true;
   }
 
-  /*if (browser) {
+  if (browser) {
     // Sync custom map settings with local storage
+    (() => {
+      const saved = localStorage.getItem("customMapSetup");
+      if (saved) {
+        mapData = MapData.fromJSON(saved);
+        mapData.sortAll();
+      }
+    })();
     $effect(() => {
-
+      localStorage.setItem("customMapSetup", JSON.stringify(mapData));
     });
-  }*/
+  }
 </script>
 
 <svelte:head>
