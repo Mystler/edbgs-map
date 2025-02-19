5ff8c33f7e48e8d324918323a7959c384b19760a
Florian Meißner <developer@mystler.eu>
2025-02-19 18:45:17 +0100

    Add default error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { base } from "$app/paths";
+  import { page } from "$app/state";
+</script>
+
+<svelte:head>
+  <title>Error {page.status}</title>
+</svelte:head>
+
+<div class="mt-2 text-center">
+  <h1>Error {page.status}</h1>
+  <p>
+    {page.error?.message}
+  </p>
+  <p>
+    <a href={base}>Go to Custom Map Setup</a>
+  </p>
+</div>
