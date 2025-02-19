7ae200e61c7f6100d89230b0e54abe95a1001e4c
Florian Meißner <developer@mystler.eu>
2025-02-19 20:51:03 +0100

    Rework custom URL reading This changes the way parameters are read so that some can be optional

--- src/lib/CustomURL.ts
+++ src/lib/CustomURL.ts
@@ -1,24 +1,33 @@
 import { base } from "$app/paths";
 import { page } from "$app/state";
 import { CurrentCamera } from "./types/CurrentCamera.svelte";
-import { MapData, type SphereType } from "./types/MapData.svelte";
+import {
+  MapData,
+  type FactionData,
+  type SphereData,
+  type SphereType,
+  type SystemData,
+} from "./types/MapData.svelte";
 
 export function createCustomURL(data: MapData) {
   const params = new URLSearchParams();
   for (const x of data.Factions) {
     params.append("fn", x.name);
-    params.append("fd", x.displayName);
+    if (x.name !== x.displayName) params.append("fd", x.displayName);
     params.append("fc", x.color);
+    if (!x.visible) params.append("fv", x.visible.toString());
   }
   for (const x of data.Systems) {
     params.append("sn", x.name);
-    params.append("sd", x.displayName);
+    if (x.name !== x.displayName) params.append("sd", x.displayName);
     params.append("sc", x.color);
+    if (!x.visible) params.append("sv", x.visible.toString());
   }
   for (const x of data.Spheres) {
     params.append("spn", x.name);
     params.append("spc", x.color);
     params.append("spt", x.type);
+    if (!x.visible) params.append("spv", x.visible.toString());
   }
   if (CurrentCamera.Position) {
     params.append("cpx", CurrentCamera.Position[0].toFixed());
@@ -36,6 +45,7 @@ export function createCustomURL(data: MapData) {
 export function readCustomURL(params: URLSearchParams): MapData {
   const data = new MapData();
 
+  // Get camera data first
   if (params.has("cpx") && params.has("cpy") && params.has("cpz")) {
     data.Camera.position = [
       parseFloat(params.get("cpx")!),
@@ -51,30 +61,57 @@ export function readCustomURL(params: URLSearchParams): MapData {
     ];
   }
 
-  if (params.has("fn") && params.has("fd") && params.has("fc")) {
-    const facs = params.getAll("fn");
-    const displayNames = params.getAll("fd");
-    const colors = params.getAll("fc");
-    facs.forEach((faction, i) => {
-      data.addFaction({ name: faction, displayName: displayNames[i], color: colors[i] });
-    });
-  }
-  if (params.has("sn") && params.has("sd") && params.has("sc")) {
-    const systems = params.getAll("sn");
-    const displayNames = params.getAll("sd");
-    const colors = params.getAll("sc");
-    systems.forEach((sys, i) => {
-      data.addSystem({ name: sys, displayName: displayNames[i], color: colors[i] });
-    });
-  }
-  if (params.has("spn") && params.has("spc") && params.has("spt")) {
-    const spheres = params.getAll("spn");
-    const colors = params.getAll("spc");
-    const sphTypes = params.getAll("spt") as SphereType[];
-    spheres.forEach((sphere, i) => {
-      data.addSphere({ name: sphere, color: colors[i], type: sphTypes[i] });
-    });
-  }
+  // In order to allow optional parameters, we iterate the parameters when setting up display data.
+  // This means, the order of the parameters is important but fields can be optional.
+  let cf: Partial<FactionData> | undefined;
+  let cs: Partial<SystemData> | undefined;
+  let csp: Partial<SphereData> | undefined;
+  params.forEach((value, key) => {
+    if (key === "fn") {
+      // New faction starts, add the previous one if it exists
+      if (cf) {
+        data.addFaction(cf);
+      }
+      // Start reading a new one now
+      cf = { name: value };
+    } else if (key === "fd" && cf) {
+      cf.displayName = value;
+    } else if (key === "fc" && cf) {
+      cf.color = value;
+    } else if (key === "fv" && cf) {
+      cf.visible = value === "true";
+    } else if (key === "sn") {
+      // New system starts, add the previous one if it exists
+      if (cs) {
+        data.addSystem(cs);
+      }
+      // Start reading a new one now
+      cs = { name: value };
+    } else if (key === "sd" && cs) {
+      cs.displayName = value;
+    } else if (key === "sc" && cs) {
+      cs.color = value;
+    } else if (key === "sv" && cs) {
+      cs.visible = value === "true";
+    } else if (key === "spn") {
+      // New sphere starts, add the previous one if it exists
+      if (csp) {
+        data.addSphere(csp);
+      }
+      // Start reading a new one now
+      csp = { name: value };
+    } else if (key === "spt" && csp) {
+      csp.type = value as SphereType;
+    } else if (key === "spc" && csp) {
+      csp.color = value;
+    } else if (key === "spv" && csp) {
+      csp.visible = value === "true";
+    }
+  });
+  // Add remaining open objects if they exist
+  if (cf) data.addFaction(cf);
+  if (cs) data.addSystem(cs);
+  if (csp) data.addSphere(csp);
 
   return data;
 }
