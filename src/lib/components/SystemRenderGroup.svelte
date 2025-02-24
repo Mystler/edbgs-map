e8ea35d56b5c93bd0ca5847e73f8dc444b01f243
Florian Meißner <developer@mystler.eu>
2025-02-24 22:43:30 +0100

    Batched grid connector rendering This removes the fancy fading for now. I may look into finding a performant way to do that later but for the sake of performance I prefer this approach.

--- src/lib/components/SystemRenderGroup.svelte
+++ src/lib/components/SystemRenderGroup.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import { T } from "@threlte/core";
-  import { InstancedMesh, MeshLineGeometry, MeshLineMaterial } from "@threlte/extras";
+  import { Instance, InstancedMesh } from "@threlte/extras";
   import { type SpanshSystem } from "../SpanshAPI";
   import SystemInstance from "./SystemInstance.svelte";
   import { DoubleSide, Vector3 } from "three";
   import { CurrentCamera } from "$lib/types/CurrentCamera.svelte";
-  import { quadIn } from "svelte/easing";
   import { HUDInfo } from "$lib/types/HUDInfo.svelte";
 
   interface Props {
@@ -27,45 +26,44 @@
 </InstancedMesh>
 
 <!-- Render grid connectors -->
-{#if HUDInfo.ShowGrid && visible}
-  {#each systems as system (system.id64)}
-    {@const pointSystem = new Vector3(system.x, system.y, -system.z)}
-    {@const pointConnector = new Vector3(
-      pointSystem.x,
-      CurrentCamera.LookAtVector.y,
-      pointSystem.z,
-    )}
-    {@const opacity =
-      1 - // Flip curve to turn fading range into opacity
-      quadIn(
-        // Easing function for our range between 0 and 1
-        Math.min(
-          // Create a 0-1 range that is the max of three fading thresholds
-          1,
-          Math.max(
-            CurrentCamera.Distance / 50, // Camera zoom distance
-            pointConnector.distanceTo(pointSystem) / 15, // Connector length
-            CurrentCamera.LookAtVector.distanceTo(pointSystem) / 30, // System to lookAt distance
-          ),
-        ),
+<InstancedMesh id="line" limit={systems.length} range={systems.length} frustumCulled={false}>
+  <T.PlaneGeometry
+    args={[0.1, 1]}
+    oncreate={(ref) => {
+      ref.translate(0, 0.5, 0);
+    }}
+  />
+  <T.MeshBasicMaterial color="#ffffff" side={DoubleSide} />
+
+  <InstancedMesh id="circle" limit={systems.length} range={systems.length} frustumCulled={false}>
+    <T.CircleGeometry args={[0.33]} />
+    <T.MeshBasicMaterial color="#ffffff" side={DoubleSide} />
+
+    {#each systems as system (system.id64)}
+      {@const pointSystem = new Vector3(system.x, system.y, -system.z)}
+      {@const pointConnector = new Vector3(
+        pointSystem.x,
+        CurrentCamera.LookAtVector.y,
+        pointSystem.z,
       )}
-    {#if opacity > 0}
-      <T.Group>
-        <T.Mesh>
-          <MeshLineGeometry points={[pointSystem, pointConnector]} />
-          <MeshLineMaterial width={0.1} color="#ffffff" {opacity} transparent depthWrite={false} />
-        </T.Mesh>
-        <T.Mesh position={pointConnector.toArray()} rotation={[-Math.PI / 2, 0, 0]}>
-          <T.CircleGeometry args={[0.33]} />
-          <T.MeshBasicMaterial
-            color="#ffffff"
-            {opacity}
-            transparent
-            depthWrite={false}
-            side={DoubleSide}
-          />
-        </T.Mesh>
-      </T.Group>
-    {/if}
-  {/each}
-{/if}
+      {@const showConnector =
+        CurrentCamera.Distance <= 50 &&
+        pointConnector.distanceTo(pointSystem) <= 15 &&
+        CurrentCamera.LookAtVector.distanceTo(pointSystem) <= 50}
+      {#if HUDInfo.ShowGrid && visible && showConnector}
+        <Instance
+          id="line"
+          position={pointConnector.toArray()}
+          rotation.y={Math.atan2(
+            CurrentCamera.PositionVector.x - pointSystem.x,
+            CurrentCamera.PositionVector.z - pointSystem.z,
+          )}
+          scale.y={pointSystem.y -
+            pointConnector.y -
+            Math.sign(pointSystem.y - pointConnector.y) * 0.1}
+        />
+        <Instance id="circle" position={pointConnector.toArray()} rotation={[-Math.PI / 2, 0, 0]} />
+      {/if}
+    {/each}
+  </InstancedMesh>
+</InstancedMesh>
