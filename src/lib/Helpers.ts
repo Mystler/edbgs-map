8ad02e94e2ad50b71d91e42e391c85bc14e1b4aa
Florian Meißner <developer@mystler.eu>
2025-02-28 17:24:58 +0100

    Fix random color length bug

--- src/lib/Helpers.ts
+++ src/lib/Helpers.ts
@@ -1,3 +1,7 @@
 export function randomColor(): string {
-  return "#" + Math.floor(Math.random() * 0xffffff).toString(16);
+  let hex = Math.floor(Math.random() * 0xffffff).toString(16);
+  while (hex.length < 6) {
+    hex = "0" + hex;
+  }
+  return "#" + hex;
 }
