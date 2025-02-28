7708fe266cfe94e8832ce6d1716e26f651d80eb7
Florian Meißner <developer@mystler.eu>
2025-02-28 18:55:57 +0100

    Simple FontAwesome wrapper

---
+++ src/lib/components/FaIcon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
+  /**
+   * This is a simple DIY wrapper for FontAwesome elements to allow treeshaking without importing the entire set.
+   */
+
+  interface Props {
+    icon: IconDefinition;
+  }
+  let { icon }: Props = $props();
+</script>
+
+{#if typeof icon.icon[4] === "string"}
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${icon.icon[0]} ${icon.icon[1]}`}>
+    <path d={icon.icon[4]} fill="currentColor" />
+  </svg>
+{/if}
+
+<style>
+  svg {
+    height: 1em;
+  }
+</style>
