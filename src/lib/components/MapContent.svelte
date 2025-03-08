b04525edf290a4b7a58f87d43aed6f937c280a31
Florian Meißner <developer@mystler.eu>
2025-03-08 17:04:29 +0100

    Fix interactivity filter on children of invisible

--- src/lib/components/MapContent.svelte
+++ src/lib/components/MapContent.svelte
@@ -7,6 +7,15 @@
 
   interactivity({
     filter: (hits) => {
+      // Only return visible elements
+      hits = hits.filter((x) => {
+        let o: typeof x.object | null = x.object;
+        while (o) {
+          if (!o.visible) return false;
+          o = o.parent;
+        }
+        return true;
+      });
       // Only return the first hit
       return hits.slice(0, 1);
     },
