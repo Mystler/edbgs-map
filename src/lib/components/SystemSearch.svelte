46de664d10832aa105478bb57b76369f659b03cd
Florian Meißner <developer@mystler.eu>
2025-03-21 15:32:14 +0100

    fix: Cap system search data list 10 systems should be enough, we don't need to add all systems to the dom

--- src/lib/components/SystemSearch.svelte
+++ src/lib/components/SystemSearch.svelte
@@ -2,6 +2,8 @@
   import { CurrentCamera, FlyToTarget } from "$lib/types/CurrentCamera.svelte";
   import { LoadedSystems } from "$lib/types/LoadedData.svelte";
   import { Vector3 } from "three";
+
+  let search = $state("");
 </script>
 
 <input
@@ -9,6 +11,7 @@
   class="opacity-10 transition-opacity duration-400 focus:opacity-100 [@media(pointer:fine)]:hover:opacity-100"
   placeholder="Go to loaded system..."
   list="knownSystems"
+  bind:value={search}
   onchange={(e) => {
     if (e.currentTarget.value) {
       const system = LoadedSystems.get(e.currentTarget.value.trim());
@@ -38,14 +41,18 @@
             posZ: -system.z + CurrentCamera.Position[2] - CurrentCamera.LookAt[2],
           });
         }
-        e.currentTarget.value = "";
+        search = "";
         e.currentTarget.blur();
       }
     }
   }}
 />
 <datalist id="knownSystems">
-  {#each LoadedSystems.keys().toArray().sort() as name (name)}
+  {#each LoadedSystems.keys()
+    .filter((name) => !search || name.toLowerCase().includes(search.toLowerCase()))
+    .toArray()
+    .slice(0, 10)
+    .sort() as name (name)}
     <option>{name}</option>
   {/each}
 </datalist>
