271e4bbec61778dfd10fd51a3b7d7edf02b67d7d
Florian Meißner <developer@mystler.eu>
2025-03-27 18:23:55 +0100

    feat: Calculate grid connectors in a shader This is actually worse performance than the previous solution, so I am going to save the approach in a branch but not push it to main.

--- src/lib/components/SystemRenderGroup.svelte
+++ src/lib/components/SystemRenderGroup.svelte
@@ -1,57 +1,93 @@
 <script module lang="ts">
-  const gcMaxCamDistance = 50;
-  const gcMaxLength = 15;
-  const gcMaxDistance = 50;
-  const gridConnectorMaterial = new RawShaderMaterial({
+  const gridConnectorMaterial = new ShaderMaterial({
     vertexShader: `
-      precision highp float;
-      attribute mat4 instanceMatrix;
-      attribute vec3 position;
-      attribute float opacity;
+      const float gcMaxCamDistance = 50.0;
+      const float gcMaxLength = 15.0;
+      const float gcMaxDistance = 50.0;
+      uniform vec3 target;
+      uniform vec3 camPosition;
+      uniform float camDistance;
       varying float f_opacity;
-      uniform mat4 projectionMatrix;
-      uniform mat4 viewMatrix;
-      uniform mat4 modelMatrix;
       void main() {
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * instanceMatrix * vec4(position, 1.0);
+        vec3 sysPos = instanceMatrix[3].xyz;
+        // Transform the instance vertices to connect to the grid plane
+        float length = target.y - sysPos.y;
+        float offset = sign(length) * 0.1;
+        float scaleY = length - offset;
+        vec3 newPosition = vec3(position.x, position.y * scaleY + offset, position.z);
+        // Calculate fading
+        float sysDist = distance(sysPos, target);
+        float fade = min(1.0, max(max(camDistance / gcMaxCamDistance, abs(length) / gcMaxLength), sysDist / gcMaxDistance));
+        float opacity = 1.0 - fade * fade * fade;
+        // Rotate around Y axis towards camera
+        mat4 rotY = mat4(1.0);
+        float angle = atan(camPosition.x - sysPos.x, camPosition.z - sysPos.z);
+        rotY[0][0] = cos(angle);
+        rotY[0][2] = -sin(angle);
+        rotY[2][0] = sin(angle);
+        rotY[2][2] = cos(angle);
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * instanceMatrix * rotY * vec4(newPosition, 1.0);
         f_opacity = opacity;
       }
     `,
     fragmentShader: `
-      precision highp float;
       varying float f_opacity;
       void main() {
         gl_FragColor = vec4(1.0, 1.0, 1.0, f_opacity);
       }
     `,
+    uniforms: {
+      target: {
+        value: CurrentCamera.LookAtVector,
+      },
+      camPosition: {
+        value: CurrentCamera.PositionVector,
+      },
+      camDistance: {
+        value: CurrentCamera.Distance,
+      },
+    },
     transparent: true,
     depthWrite: false,
   });
   gridConnectorMaterial.side = DoubleSide;
+
+  function onCameraChange() {
+    const target = gridConnectorMaterial.uniforms.target as Uniform<Vector3>;
+    target.value.set(...CurrentCamera.LookAt);
+    const camPosition = gridConnectorMaterial.uniforms.camPosition as Uniform<Vector3>;
+    camPosition.value.set(...CurrentCamera.Position);
+    const camDistance = gridConnectorMaterial.uniforms.camDistance as Uniform<number>;
+    camDistance.value = CurrentCamera.Distance;
+  }
+  $effect.root(() => {
+    $effect(() => {
+      CurrentCamera.Controls?.addEventListener("change", onCameraChange);
+      untrack(() => {
+        onCameraChange();
+      });
+      return () => {
+        CurrentCamera.Controls?.removeEventListener("change", onCameraChange);
+      };
+    });
+  });
 </script>
 
 <script lang="ts">
   import { T } from "@threlte/core";
-  import { InstancedMesh } from "@threlte/extras";
+  import { Instance, InstancedMesh } from "@threlte/extras";
   import { type SpanshSystem } from "../SpanshAPI";
   import SystemInstance from "./SystemInstance.svelte";
   import {
-    CircleGeometry,
     DoubleSide,
-    DynamicDrawUsage,
-    InstancedBufferAttribute,
-    PlaneGeometry,
-    RawShaderMaterial,
     Vector3,
     InstancedMesh as ThreeInstancedMesh,
-    Object3D,
     ShaderMaterial,
     Color,
     Uniform,
   } from "three";
   import { CurrentCamera } from "$lib/types/CurrentCamera.svelte";
   import { HUDInfo } from "$lib/types/HUDInfo.svelte";
-  import { cubicIn } from "svelte/easing";
   import TriangleShape from "./Shapes/TriangleShape.svelte";
   import StarShape from "./Shapes/StarShape.svelte";
   import { untrack } from "svelte";
@@ -81,10 +117,9 @@
       }
     `,
     fragmentShader: `
-      precision highp float;
       varying vec3 f_color;
       void main() {
-        gl_FragColor = vec4(f_color.xyz, 1.0);
+        gl_FragColor = vec4(f_color, 1.0);
       }
     `,
   });
@@ -95,105 +130,6 @@
     uColor.value.set(new Color(color).convertLinearToSRGB());
   });
 
-  interface GridConnector {
-    pointSystem: Vector3;
-    pointConnector: Vector3;
-    opacity: number;
-  }
-  function calculateGridConnectors(): GridConnector[] {
-    if (CurrentCamera.Distance >= gcMaxCamDistance) return [];
-    const gcSystems: GridConnector[] = [];
-    for (const system of systems) {
-      const pointSystem = new Vector3(system.x, system.y, -system.z);
-      const pointConnector = new Vector3(
-        pointSystem.x,
-        CurrentCamera.LookAtVector.y,
-        pointSystem.z,
-      );
-      const length = Math.abs(pointSystem.y - pointConnector.y);
-      if (length >= gcMaxLength) continue;
-      const distance = CurrentCamera.LookAtVector.distanceTo(pointConnector);
-      if (distance >= gcMaxDistance) continue;
-      const opacity =
-        1 - // Flip curve to turn fading range into opacity
-        cubicIn(
-          // Easing function for our range between 0 and 1
-          Math.min(
-            // Create a 0-1 range that is the max of three fading thresholds
-            1,
-            Math.max(
-              CurrentCamera.Distance / gcMaxCamDistance, // Camera zoom distance
-              length / gcMaxLength, // Connector length
-              distance / 50, // Connector to lookAt distance
-            ),
-          ),
-        );
-      gcSystems.push({ pointSystem, pointConnector, opacity });
-    }
-    return gcSystems;
-  }
-
-  const opacities = new Float32Array(systems.length);
-  const opacityBuffer = new InstancedBufferAttribute(opacities, 1);
-  opacityBuffer.setUsage(DynamicDrawUsage);
-
-  const gcLine = new PlaneGeometry(0.1, 1);
-  gcLine.translate(0, 0.5, 0);
-  gcLine.setAttribute("opacity", opacityBuffer);
-  const gcCircle = new CircleGeometry(0.33);
-  gcCircle.setAttribute("opacity", opacityBuffer);
-  const gcLineMesh = new ThreeInstancedMesh(gcLine, gridConnectorMaterial, systems.length);
-  const gcCircleMesh = new ThreeInstancedMesh(gcCircle, gridConnectorMaterial, systems.length);
-  const gcLineDummy = new Object3D();
-  const gcCircleDummy = new Object3D();
-
-  let lastConnectors = 0;
-  function onCameraChange() {
-    if (!HUDInfo.ShowGrid || !visible) return;
-    const gridConnectors = calculateGridConnectors();
-    gridConnectors.forEach((gc, i) => {
-      opacities[i] = gc.opacity;
-      gcLineDummy.position.set(gc.pointConnector.x, gc.pointConnector.y, gc.pointConnector.z);
-      gcLineDummy.rotation.y = Math.atan2(
-        CurrentCamera.PositionVector.x - gc.pointSystem.x,
-        CurrentCamera.PositionVector.z - gc.pointSystem.z,
-      );
-      gcLineDummy.scale.y =
-        gc.pointSystem.y -
-        gc.pointConnector.y -
-        Math.sign(gc.pointSystem.y - gc.pointConnector.y) * 0.1;
-      gcLineDummy.updateMatrix();
-      gcLineMesh.setMatrixAt(i, gcLineDummy.matrix);
-      gcCircleDummy.position.set(gc.pointConnector.x, gc.pointConnector.y, gc.pointConnector.z);
-      gcCircleDummy.rotation.x = -Math.PI / 2;
-      gcCircleDummy.updateMatrix();
-      gcCircleMesh.setMatrixAt(i, gcCircleDummy.matrix);
-    });
-    if (gridConnectors.length > 0 || lastConnectors > 0) {
-      opacityBuffer.needsUpdate = true;
-      gcLineMesh.count = gridConnectors.length;
-      gcLineMesh.instanceMatrix.needsUpdate = true;
-      gcLineMesh.computeBoundingSphere();
-      gcCircleMesh.count = gridConnectors.length;
-      gcCircleMesh.instanceMatrix.needsUpdate = true;
-      gcCircleMesh.computeBoundingSphere();
-    }
-    lastConnectors = gridConnectors.length;
-  }
-
-  $effect(() => {
-    // Force update when visibility is back on
-    if (HUDInfo.ShowGrid && visible) {
-      untrack(() => onCameraChange());
-    }
-  });
-
-  $effect(() => {
-    CurrentCamera.Controls?.addEventListener("change", onCameraChange);
-    return () => {
-      CurrentCamera.Controls?.removeEventListener("change", onCameraChange);
-    };
-  });
   let vMesh = $state() as ThreeInstancedMesh;
   let rMesh = $state() as ThreeInstancedMesh;
 </script>
@@ -250,5 +186,33 @@
 </InstancedMesh>
 
 <!-- Render grid connectors -->
-<T is={gcLineMesh} visible={HUDInfo.ShowGrid && visible} />
-<T is={gcCircleMesh} visible={HUDInfo.ShowGrid && visible} />
+<InstancedMesh id="line" visible={HUDInfo.ShowGrid && visible} update={false} frustumCulled={false}>
+  <T.PlaneGeometry
+    args={[0.1, 1]}
+    oncreate={(ref) => {
+      ref.translate(0, 0.5, 0);
+    }}
+  />
+  <T is={gridConnectorMaterial} />
+  <InstancedMesh
+    id="circle"
+    visible={HUDInfo.ShowGrid && visible}
+    update={false}
+    frustumCulled={false}
+  >
+    <T.CircleGeometry
+      args={[0.33]}
+      oncreate={(ref) => {
+        ref.rotateX(-Math.PI / 2);
+        ref.translate(0, 1, 0);
+      }}
+    />
+    <T is={gridConnectorMaterial} />
+    {#each systems as system (system.name)}
+      <T.Group position={[system.x, system.y, -system.z]}>
+        <Instance id="line" />
+        <Instance id="circle" />
+      </T.Group>
+    {/each}
+  </InstancedMesh>
+</InstancedMesh>
