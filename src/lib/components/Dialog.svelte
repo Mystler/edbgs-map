d2c2feae44393692644ee1435680d4bacb6ee07c
Florian Meißner <developer@mystler.eu>
2025-05-02 17:46:37 +0200

    feat: Improve Dialog component Allow immediate showing via property, onclose event, and reactive isOpen state.

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -1,18 +1,25 @@
 <script lang="ts">
   import { faXmark } from "@fortawesome/free-solid-svg-icons";
   import FaIcon from "./FaIcon.svelte";
+  import { on } from "svelte/events";
+  import { onMount, type Snippet } from "svelte";
 
-  let { children } = $props();
+  interface Props {
+    showImmediately?: boolean;
+    children: Snippet;
+    onclose?: () => void;
+  }
+  let { children, showImmediately = false, onclose = () => {} }: Props = $props();
 
   let dialog = $state() as HTMLDialogElement;
+  let opened = $state(false);
 
-  export const isOpen = () => dialog.open;
+  export const isOpen = () => opened;
   export function show() {
     dialog.showModal();
-    dialog.addEventListener("click", closeClickBackdrop);
+    opened = true;
   }
   export function close() {
-    dialog.removeEventListener("click", closeClickBackdrop);
     dialog.close();
   }
   function closeClickBackdrop(e: MouseEvent) {
@@ -20,9 +27,22 @@
       close();
     }
   }
+
+  onMount(() => {
+    if (showImmediately) show();
+    const off = on(dialog, "click", closeClickBackdrop);
+    return off;
+  });
 </script>
 
-<dialog bind:this={dialog} class="m-auto bg-transparent outline-none backdrop:backdrop-blur-xs">
+<dialog
+  bind:this={dialog}
+  class="m-auto bg-transparent outline-none backdrop:backdrop-blur-xs"
+  onclose={() => {
+    opened = false;
+    onclose();
+  }}
+>
   <button type="button" class="absolute top-2 right-2 p-2" onclick={close} aria-label="Close"
     ><FaIcon icon={faXmark} /></button
   >
