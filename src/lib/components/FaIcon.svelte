86b27d3d46f37a4b070b5c2178fedf222f52c915
Florian Meißner <developer@mystler.eu>
2025-05-02 17:44:31 +0200

    feat: Allow svg props on FaIcon wrapper

--- src/lib/components/FaIcon.svelte
+++ src/lib/components/FaIcon.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+  import type { SVGAttributes } from "svelte/elements";
 
   /**
    * This is a simple DIY wrapper for FontAwesome elements to allow treeshaking without importing the entire set.
    */
 
-  interface Props {
+  interface Props extends SVGAttributes<SVGElement> {
     icon: IconDefinition;
   }
-  let { icon }: Props = $props();
+  let { icon, ...rest }: Props = $props();
 </script>
 
 {#if typeof icon.icon[4] === "string"}
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${icon.icon[0]} ${icon.icon[1]}`}>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${icon.icon[0]} ${icon.icon[1]}`} {...rest}>
     <path d={icon.icon[4]} fill="currentColor" />
   </svg>
 {/if}
