8a2eed0db89c6f4e1d0658adf5dbebafa21d6073
Florian Meißner <developer@mystler.eu>
2025-05-29 17:38:45 +0200

    fix: Last Tick calculation on same day

--- src/lib/Helpers.ts
+++ src/lib/Helpers.ts
@@ -63,12 +63,12 @@ export function calculateGeometricMedian(points: SpanshSystem[]) {
 
 export function getLastPPTickDate() {
   const lastTick = new Date();
-  const day = lastTick.getDay();
-  if (day === 4) {
+  const day = lastTick.getUTCDay();
+  if (day === 4 && lastTick.getUTCHours() < 7) {
     lastTick.setDate(lastTick.getDate() - 7);
   } else if (day > 4) {
     lastTick.setDate(lastTick.getDate() - (day - 4));
-  } else {
+  } else if (day < 4) {
     lastTick.setDate(lastTick.getDate() - (day + 3));
   }
   lastTick.setUTCHours(7, 0, 0, 0);
