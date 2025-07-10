9ada13fcd062aba08bc8c7865911158f85aa90db
Florian Meißner <developer@mystler.eu>
2025-07-10 19:53:13 +0200

    feat: Use own cache for PP data if available

--- src/routes/api/power/system/[id64]/+server.ts
+++ src/routes/api/power/system/[id64]/+server.ts
@@ -7,13 +7,20 @@ export async function GET({ params, setHeaders }) {
     "cache-control": "max-age=900",
   });
   const id64 = parseInt(params.id64);
+  // Check if we have a result cached from spansh
   const cachedResult = await getCache(`edbgs-map:pp-data:${id64}`);
   if (cachedResult) {
     const system: SpanshDumpPPData = JSON.parse(cachedResult);
     return json(system);
-  } else {
-    const system = await fetchSystemPPData(id64);
-    setTimedCache(`edbgs-map:pp-data:${id64}`, JSON.stringify(system), 3600);
+  }
+  // Check if we have a result cached from our EDDN listener
+  const ownSystemCache = await getCache(`edbgs-map:pp-alert:${id64}`);
+  if (ownSystemCache) {
+    const system: SpanshDumpPPData = JSON.parse(ownSystemCache);
     return json(system);
   }
+  // Fetch latest data from Spansh and cache that
+  const system = await fetchSystemPPData(id64);
+  setTimedCache(`edbgs-map:pp-data:${id64}`, JSON.stringify(system), 3600);
+  return json(system);
 }
