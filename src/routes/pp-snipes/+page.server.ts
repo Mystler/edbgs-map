1b16bd55095418aa5e3bf5c5207616d98e865c4c
Florian Meißner <developer@mystler.eu>
2025-07-25 15:00:02 +0200

    feat: Allow system search URL param on snipes

--- src/routes/pp-snipes/+page.server.ts
+++ src/routes/pp-snipes/+page.server.ts
@@ -1,8 +1,15 @@
 import { dbGetAll } from "$lib/server/DB.js";
 import { error } from "@sveltejs/kit";
 
-export async function load({ depends }) {
+export async function load({ depends, url }) {
   depends("app:pp-snipes");
+
+  let query = "SELECT * FROM snipe_history ORDER BY id DESC LIMIT 500";
+  let params: string[] = [];
+  if (url.searchParams.has("system")) {
+    query = "SELECT * FROM snipe_history WHERE system = ? ORDER BY id DESC LIMIT 500";
+    params = [url.searchParams.get("system")!];
+  }
 
   const q = await dbGetAll<{
     id: number;
@@ -12,7 +19,7 @@ export async function load({ depends }) {
     amount: number;
     old_dump: string | null;
     new_dump: string;
-  }>("SELECT * FROM snipe_history ORDER BY id DESC LIMIT 500");
+  }>(query, ...params);
 
   if (!q) error(404, "Could not find snipe history data!");
 
