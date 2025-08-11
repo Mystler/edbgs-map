03b52e238d1eb13f411ae67d4b2b54a51e4121e5
Florian Meißner <developer@mystler.eu>
2025-08-11 15:04:19 +0200

    fix: Filter decay from alert selection This only filters the systems that are picked up to be shown in the list, it does not affect their total merit count when they are displayed once they have more than 10k player CP.

--- src/routes/api/power/alerts/+server.ts
+++ src/routes/api/power/alerts/+server.ts
@@ -1,4 +1,4 @@
-import { getLastPPTickDate } from "$lib/Powerplay";
+import { calculatePPControlSegments, getDecayValue, getLastPPTickDate } from "$lib/Powerplay";
 import type { SpanshDumpPPData } from "$lib/SpanshAPI";
 import { getAllCacheMatching } from "$lib/server/ValkeyCache";
 import { json } from "@sveltejs/kit";
@@ -6,15 +6,30 @@ import { json } from "@sveltejs/kit";
 export async function GET() {
   // This cycle or up to last 48h if before
   const cutoff = Math.min(new Date().valueOf() - 172_800_000, getLastPPTickDate().valueOf());
-  const cachedResult = await getAllCacheMatching<SpanshDumpPPData>(
-    "edbgs-map:pp-alert:*",
-    (x) =>
-      new Date(x.date).valueOf() > cutoff &&
-      ((x.powerStateReinforcement !== undefined &&
-        x.powerStateUndermining !== undefined &&
-        x.powerStateReinforcement + x.powerStateUndermining >= 10000) || // >=10k total CP done
-        (x.powerStateControlProgress !== undefined && x.powerStateControlProgress < 0) || // Danger, negative progress that would cause tier drop
-        (x.powerConflictProgress !== undefined && x.powerConflictProgress.some((y) => y.progress >= 0.3))), // >=30% progress
-  );
+  const cachedResult = await getAllCacheMatching<SpanshDumpPPData>("edbgs-map:pp-alert:*", (x) => {
+    // Ignore systems older than cutoff
+    if (new Date(x.date).valueOf() < cutoff) return false;
+
+    if (
+      x.powerStateControlProgress !== undefined &&
+      x.powerStateReinforcement !== undefined &&
+      x.powerStateUndermining !== undefined
+    ) {
+      // Always alert on tier drop
+      if (x.powerStateControlProgress < 0) return true;
+      // Calculate expected decay if in same tier (to avoid capping offsets)
+      let expectedDecay = 0;
+      if (x.powerStateControlProgress > 0 && x.powerStateControlProgress < 100) {
+        const { startProgress } = calculatePPControlSegments(x);
+        expectedDecay = getDecayValue(startProgress, x.powerState);
+      }
+      // Alert on >= 10k activity with decay removed
+      if (x.powerStateReinforcement + (x.powerStateUndermining - expectedDecay) >= 10000) return true;
+    }
+
+    // Show acquisitions if >= 30% progress
+    if (x.powerConflictProgress !== undefined && x.powerConflictProgress.some((y) => y.progress >= 0.3)) return true;
+    return false;
+  });
   return json(cachedResult);
 }
