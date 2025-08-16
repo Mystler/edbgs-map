c8edb9c2f01267c57bfcb1001fbabb2f4cea8e36
Florian Meißner <developer@mystler.eu>
2025-08-17 00:12:45 +0200

    fix: Move PP tick time 5 mins later This should fix data coming in at cycle time when servers go down a bit late

--- src/lib/Powerplay.ts
+++ src/lib/Powerplay.ts
@@ -3,14 +3,14 @@ import type { SpanshDumpPPData } from "./SpanshAPI";
 export function getLastPPTickDate() {
   const lastTick = new Date();
   const day = lastTick.getUTCDay();
-  if (day === 4 && lastTick.getUTCHours() < 7) {
+  if (day === 4 && (lastTick.getUTCHours() < 7 || (lastTick.getUTCHours() === 7 && lastTick.getUTCMinutes() < 5))) {
     lastTick.setDate(lastTick.getDate() - 7);
   } else if (day > 4) {
     lastTick.setDate(lastTick.getDate() - (day - 4));
   } else if (day < 4) {
     lastTick.setDate(lastTick.getDate() - (day + 3));
   }
-  lastTick.setUTCHours(7, 0, 0, 0);
+  lastTick.setUTCHours(7, 5, 0, 0);
   return lastTick;
 }
 
