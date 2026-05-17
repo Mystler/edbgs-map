b0a6d4db056acfc5d24de5c6dfc6217634781166
Mystler <161936+Mystler@users.noreply.github.com>
2026-05-17 16:05:18 +0200

    fix: Sorting fix

--- src/lib/components/CycleStats.svelte
+++ src/lib/components/CycleStats.svelte
@@ -440,7 +440,7 @@
                     : 0,
                 ] as const,
             )
-            .toSorted((a, b) => b[2] - a[2])}
+            .toSorted((a, b) => b[2] + b[3] - a[2] - a[3])}
           <Chart
             type="bar"
             stack={true}
